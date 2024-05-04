27edb7ba620f8db74a249db737670482c1a9e5c0
Tolemak <60557277+Tolemak@users.noreply.github.com>
2024-05-04 16:30:35 +0000

    Deploying to gh-pages from @ Tolemak/portfolio@ebd12e2392c50dd3f9cacea45542bf72a6c5689b 🚀

---
+++ _app/immutable/chunks/projects.31b68941.js
@@ -0,0 +1,1 @@
+import{A as o}from"./UIcon.0bcfd049.js";import{g as s}from"./skills.0ce812ef.js";const n=[{slug:"soon",color:"#5e95e3",description:"soon",shortDescription:"soon",links:[{to:"",label:"GitHub"}],logo:o.Unknown,name:"Soon",period:{from:new Date},skills:s(),type:"Soon"}],i="Projects";export{n as i,i as t};
