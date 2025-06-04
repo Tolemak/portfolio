5e6535dae90cd32513de99c7739e03ff06fd8e1a
Tolemak <60557277+Tolemak@users.noreply.github.com>
2025-06-04 17:19:57 +0000

    Deploying to gh-pages from @ Tolemak/portfolio@d0991e544f0d747cabdcabb52ae774d1ae89fbc3 🚀

---
+++ _app/immutable/chunks/skills.c5841c29.js
@@ -0,0 +1,74 @@
+import{A as a}from"./UIcon.83f396f4.js";function g(e,...t){const s=Object.assign({},e);return Object.keys(s).forEach(r=>{t.includes(r)&&delete s[r]}),s}const i=e=>e,p=[i({name:"Programming Languages",slug:"pro-lang"}),i({name:"Frameworks",slug:"framework"}),i({name:"Libraries",slug:"library"}),i({name:"Langauges",slug:"lang"}),i({name:"Databases",slug:"db"}),i({name:"ORMs",slug:"orm"}),i({name:"DevOps",slug:"devops"}),i({name:"Testing",slug:"test"}),i({name:"Dev Tools",slug:"devtools"}),i({name:"Markup & Style",slug:"markup-style"}),i({name:"Design",slug:"design"}),i({name:"Soft Skills",slug:"soft"})],n=e=>{const t=g(e,"category");return e.category&&(t.category=p.find(s=>s.slug===e.category)),t},c=[n({slug:"js",color:"yellow",description:`<p>
+			1. JavaScript UX Specialist:<br>
+			   Proficient in leveraging JavaScript to craft seamless user experiences, I specialize in building dynamic interfaces that engage and delight users. My expertise lies in creating fluid transitions, interactive elements, and intuitive navigation, enhancing overall usability and satisfaction.
+			</p>
+			<br>
+			<p>
+			2. Data Preloading Maestro:<br>
+			   With a knack for optimizing performance, I excel in using JavaScript to preload data, ensuring swift access to content and resources. Whether it's prefetching images, loading essential assets, or fetching data asynchronously, I employ efficient techniques to minimize load times and enhance the user experience.
+			<p>
+			<br>
+			</p>
+			3. Interactive Frontend Developer:<br>
+			   As an adept JavaScript developer, I focus on building interactive frontend experiences that captivate users. From designing responsive layouts to implementing animated transitions and real-time updates, I leverage JavaScript to create engaging interfaces that keep users immersed and engaged.
+			</p>
+			<br>
+			<p>
+			4. Efficient Data Loader:<br>
+			   Specializing in data preloading techniques, I use JavaScript to seamlessly fetch and load data ahead of time, optimizing application performance. By prefetching critical resources, caching frequently accessed data, and utilizing lazy loading strategies, I ensure that users have instant access to the information they need, resulting in a smooth and responsive user experience.
+			</p>`,logo:a.JavaScript,name:"Javascript",category:"pro-lang"}),n({slug:"php",color:"blue",description:`<p>1. PHP REST API Architect:<br>
+			   Proficient in PHP for REST API integrations, I specialize in designing and developing robust API endpoints to facilitate seamless communication between applications. Leveraging PHP frameworks like Laravel or Symfony, I ensure secure authentication, data validation, and error handling, enabling smooth data exchange and interoperability.
+			</p>
+			<br>
+			<p>	
+			2. Data Preprocessing Prodigy:<br>
+			   With PHP as my tool of choice, I excel in preprocessing data before SQL operations, ensuring data consistency and integrity. Whether it's sanitizing user input, validating data formats, or transforming data structures, I leverage PHP's versatile functions and libraries to prepare data for efficient storage and retrieval in SQL databases.
+			</p>
+			<br>
+			<p>	
+			3. API Integration Expert:<br>
+			   As a skilled PHP developer, I focus on integrating RESTful APIs seamlessly into applications, enabling communication with external services and systems. Using PHP's cURL extension or HTTP client libraries, I establish connections, send requests, and handle responses, ensuring smooth data exchange and synchronization between disparate systems.
+			</p>
+			<br>
+			<p>	
+			4. SQL Data Pipeline Maestro:<br>
+			   Specializing in PHP for preprocessing data before SQL operations, I create efficient data pipelines to optimize database performance and reliability. Leveraging PHP's data manipulation capabilities, I cleanse, transform, and validate data before inserting or updating records in SQL databases, ensuring data quality and consistency.
+			</p>`,logo:a.PHP,name:"PHP",category:"pro-lang"}),n({slug:"python",color:"blue",description:`<p>1. Python Data Wizard:<br>
+			   Proficient in Python for data processing, I specialize in extracting insights from raw datasets, transforming them into meaningful information. Whether it's cleaning, analyzing, or visualizing data, I leverage Python's powerful libraries like Pandas and NumPy to efficiently handle large datasets and derive actionable insights.
+			   </p>
+			   <br>
+			   <p>			
+			2. Automation Script Guru:<br>
+			   With Python as my tool of choice, I excel in creating automation scripts to streamline repetitive tasks and workflows. From automating file management and data entry to scheduling routine processes, I harness Python's simplicity and versatility to save time and increase productivity.
+			   </p>
+			   <br>
+			   <p>
+			3. Data Processing Maestro:<br>
+			   As a skilled Python developer, I focus on processing and manipulating data with precision and efficiency. Using libraries like pandas, numpy, and scikit-learn, I clean, preprocess, and analyze data to extract valuable insights and drive informed decision-making.
+			   </p>
+			   <br>
+			   <p>
+			4. Efficient Script Developer:<br>
+			   Specializing in automation and scripting with Python, I create tailored solutions to simplify complex tasks and workflows. Whether it's automating system maintenance, deploying software updates, or orchestrating workflows, I leverage Python's rich ecosystem of libraries and frameworks to develop robust and scalable automation scripts.
+			   </p>`,logo:a.Python,name:"Python",category:"pro-lang"}),n({slug:"sql",color:"blue",description:`<p>1. SQL Virtuoso Across Platforms:
+		   Proficient in SQL Anywhere, MySQL, and PostgreSQL, I adeptly navigate diverse database environments to deliver optimized solutions tailored to specific needs. From performance tuning to schema design, I leverage my expertise across platforms to ensure efficient data management and retrieval.
+		   </p>
+		   <br>
+		   <p>
+		2. Advanced Query Architect:<br>
+		   With a knack for crafting intricate queries, I specialize in creating advanced SQL functions and queries that extract valuable insights from complex datasets. Whether it's aggregating data, performing joins, or implementing window functions, I employ SQL's powerful features to derive meaningful results efficiently.
+		   </p>
+		   <br>
+		   <p>
+		3. Schema Management Maestro:<br>
+		   As a skilled database administrator, I excel in managing database schemas across SQL Anywhere, MySQL, and PostgreSQL environments. From designing schema structures to optimizing indexes and constraints, I ensure data integrity and scalability while adhering to best practices for efficient database management.
+		   </p>
+		   <br>
+		   <p>
+		4. Performance Optimization Guru:<br>
+		   With a focus on performance optimization, I fine-tune SQL queries and database configurations to maximize efficiency and responsiveness. Leveraging my deep understanding of SQL execution plans and indexing strategies, I optimize query performance across different database platforms to deliver optimal user experiences and minimize resource consumption.
+		   </p>
+		   <br>
+		   <p>
+		5. Cross-Platform Database Architect:<br>
+		   Specializing in database architecture across SQL Anywhere, MySQL, and PostgreSQL, I design robust schemas and data models that meet diverse business requirements. From conceptualization to implementation, I leverage my expertise to create scalable and maintainable database solutions that drive business growth and innovation.</p>`,logo:a.SAP,name:"SQL",category:"db"}),n({slug:"html",color:"orange",description:"Proficient in HTML, I specialize in creating web pages tailored to meet diverse user needs and preferences. With a focus on user requirements, I excel in translating ideas into HTML structures that accurately represent desired designs and functionalities. Whether it's crafting custom layouts, implementing interactive elements, or optimizing for accessibility, I leverage HTML's versatility to deliver web solutions that align with user expectations.",logo:a.HTML,name:"HTML",category:"markup-style"}),n({slug:"css",color:"blue",description:"Proficient in CSS, I specialize in crafting custom styles tailored to diverse user requirements. With attention to detail, I excel in customizing CSS to match specific user preferences and brand aesthetics. Skilled in creating responsive designs that adapt seamlessly to various screen sizes and devices, I prioritize usability and accessibility, creating intuitive and inclusive CSS styles for enhanced user experience. Specializing in dynamic user interfaces, I leverage CSS animations and interactions to create engaging web experiences.",logo:a.CSS,name:"CSS",category:"markup-style"}),n({slug:"symfony",color:"white",description:"Skilled in PHP Symfony, I specialize in utilizing this versatile framework to develop web applications customized to various user needs. With a sharp focus on routing strategies, I excel in designing webpage pathways that ensure seamless navigation and functionality. Whether it involves crafting unique controllers, managing route parameters, or optimizing for top performance, I leverage Symfony's extensive capabilities to deliver web solutions that perfectly align with user expectations. My expertise lies in seamlessly blending Symfony's robust features with innovative problem-solving to deliver sophisticated and user-centric web experiences.",logo:a.Symfony,name:"Symfony",category:"framework"}),n({slug:"bootstrap",color:"violet",description:"Skilled in Bootstrap, I specialize in harnessing this robust front-end framework to craft compelling user experiences. With a sharp focus on design coherence and adaptability, I excel in creating visually striking and intuitive interfaces. Whether it entails constructing responsive layouts, styling elements, or integrating interactive features, I leverage Bootstrap's rich array of pre-designed components and CSS utilities to expedite development and guarantee seamless cross-browser performance. My expertise lies in seamlessly amalgamating Bootstrap's versatile grid system and elements to deliver contemporary and immersive web experiences that captivate users.",logo:a.Bootstrap,name:"Bootstrap",category:"framework"}),n({slug:"twig",color:"green",description:"Skilled in Twig, I excel not only in crafting HTML content but also in formatting data and creating macros to enhance template functionality. With Twig's robust capabilities, I specialize in transforming and presenting data in a visually appealing and structured manner within HTML templates. Whether it involves formatting dates, numbers, or text strings, I leverage Twig's built-in filters and functions to ensure consistency and readability across various data types. Additionally, I am proficient in creating macros to encapsulate reusable logic and functionality within templates, promoting code reusability and maintainability. By harnessing Twig's powerful features for data formatting and macro creation, I am able to streamline template development and create dynamic and feature-rich web interfaces that meet the evolving needs of web projects.",logo:a.Twig,name:"Twig",category:"library"}),n({slug:"vscode",color:"blue",description:"<p>Visual Studio Code (VS Code) stands as my favored and primary development tool, seamlessly integrating into my workflow to enhance productivity and streamline coding tasks. With an array of customizable features and an extensive library of plugins, I have tailored VS Code to suit my specific requirements, optimizing it for efficient and personalized development experiences.</p><br><p>I rely on VS Code's intuitive interface and versatile functionality to tackle a wide range of coding tasks with ease. From writing and debugging code to managing projects and collaborating with teammates, VS Code serves as my go-to platform for all my development needs.</p><br><p>What sets VS Code apart for me is its vast ecosystem of plugins and extensions, which I leverage extensively to tailor the editor to my preferences. Whether it's integrating linters and code formatters to maintain code quality, or adding themes and color schemes for a personalized aesthetic, I have fine-tuned VS Code to align perfectly with my workflow and coding style.</p><br><p>Moreover, VS Code's robust support for various programming languages and frameworks ensures that I can work seamlessly across different projects and technologies without sacrificing efficiency or consistency. Whether I'm working on web development, data science, or cloud computing, VS Code provides the necessary tools and integrations to tackle any coding challenge effectively.</p><br><p>Overall, Visual Studio Code stands as my indispensable companion in the world of software development, empowering me to write clean, maintainable code and build innovative solutions with speed and precision. Its flexibility, extensibility, and intuitive interface make it an essential tool in my arsenal, enabling me to unleash my creativity and productivity to the fullest extent.</p>",logo:a.VSCode,name:"Visual Studio Code",category:"devtools"})],u="Skills",f=(...e)=>c.filter(t=>e.includes(t.slug)),y=e=>{const t=[],s=[];return c.forEach(r=>{if(e.trim()&&!r.name.toLowerCase().includes(e.trim().toLowerCase()))return;if(!r.category){s.push(r);return}let o=t.find(d=>{var l;return d.category.slug===((l=r.category)==null?void 0:l.slug)});o||(o={items:[],category:r.category},t.push(o)),o.items.push(r)}),s.length!==0&&t.push({category:{name:"Others",slug:"others"},items:s}),t};export{y as a,f as g,c as i,u as t};
